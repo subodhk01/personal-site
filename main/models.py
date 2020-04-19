1372302c924835a24786f411c58bb7c4f0645c04
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-19 18:56:16 +0530

    added __str__ returns for the models

--- main/models.py
+++ main/models.py
@@ -6,7 +6,13 @@ class Info(models.Model):
     info = models.CharField(max_length=300)
     quote = models.CharField(max_length=300)
 
+    def __str__(self):
+        return str(self.id + ": " + self.name)
+
 class Social(models.Model):
     name = models.CharField(max_length=30)
     image_url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
 
