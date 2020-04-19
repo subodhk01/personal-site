415992b659eedfb9855205486b56706019bb46f9
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-19 18:11:58 +0530

    max_length integer :-(

--- main/models.py
+++ main/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Info(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length="20")
-    info = models.CharField(max_length="300")
-    quote = models.CharField(max_length="300")
+    name = models.CharField(max_length=20)
+    info = models.CharField(max_length=300)
+    quote = models.CharField(max_length=300)
 
 class Social(models.Model):
-    name = models.CharField(max_length="30")
+    name = models.CharField(max_length=30)
     image_url = models.URLField(max_length=200)
 
