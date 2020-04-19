7299dfa56421da0d102e2157adc5f10cb7df93e0
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-19 18:16:28 +0530

    ran collectstatic

--- subodhverma/settings.py
+++ subodhverma/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static') ,)
+#STATIC_ROOT = 'static'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
