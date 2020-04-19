317a857b44fc828d58eff06bcb45fa4865f1a1d8
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-19 18:54:33 +0530

    removed url field

--- main/models.py
+++ main/models.py
@@ -8,5 +8,5 @@ class Info(models.Model):
 
 class Social(models.Model):
     name = models.CharField(max_length=30)
-    image_url = models.URLField(max_length=200)
+    image_url = models.CharField(max_length=200)
 
