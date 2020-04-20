8b3b0e6d68f090e6db14cc3c3b55030f10a16fd5
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-20 18:21:29 +0530

    changed favicon

--- main/views.py
+++ main/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    subodh = Info.objects.get(pk=1)
+    socials = Social.objects.all()
+    context = {
+        'subodh':subodh,
+        'socials':socials
+    }
+    return render(request, 'index.html', context)
